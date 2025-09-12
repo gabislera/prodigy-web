5dc87acbf428bef49f88b23f150816cd377dcc0b
Gabriela <gabriela.carniel@sellflux.com.br>
2025-09-12 14:09:55 -0300

    shadcn setup

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,5 +13,10 @@ export default defineConfig({
       target: 'react',
       autoCodeSplitting: true,
     })
-  ]
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
