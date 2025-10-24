cdbcf2c40a389b0376773e928f83b6363f77df7a
Gabriela <gabriela.carniel@sellflux.com.br>
2025-10-24 11:23:05 -0300

    refactor: remove moment.js and react big calendar

--- src/components/calendar/index.ts
+++ src/components/calendar/index.ts
@@ -1,4 +1,3 @@
 export { AIDialog } from "./ai-dialog";
-export { CustomToolbar } from "./custom-toolbar";
 export { FiltersSidebar } from "./filters-sidebar";
 export { TasksSidebar } from "./tasks-sidebar";
